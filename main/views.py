953e77b8bdbdc62600cd2a5755b7a9fd4497400c
The Taha Yaseen <thetahayaseen@gmail.com>
2023-11-04 02:04:51 +0500

    Registration Done

--- main/views.py
+++ main/views.py
@@ -1,12 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, logout, authenticate
 
 # Create your views here.
 def register_view(request):
 
     form = UserCreationForm()
+    error = ""
 
-    context = {"form": form}
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect("home")
+        else:
+            error = "An Error Occured During Registration"
+
+    context = {"form": form, "error": error}
 
     return render(request, "main/register.html", context)
