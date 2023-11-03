a8330c025d9dc0c602006d860a8a14039c226636
The Taha Yaseen <thetahayaseen@gmail.com>
2023-11-04 02:18:35 +0500

    Login Done

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 
 
 urlpatterns = [
-    path("register", views.register_view, name = "register")
+    path("register", views.register_view, name = "register"),
+    path("login", views.login_view, name = "login"),
 ]
--- main/views.py
+++ main/views.py
@@ -23,3 +23,31 @@ def register_view(request):
     context = {"form": form, "error": error}
 
     return render(request, "main/register.html", context)
+
+def login_view(request):
+    
+    if request.user.is_authenticated:
+        return redirect("home")
+
+    error = ""
+
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        try:
+            user = Users.object.get(username = username)
+        except User.DoesNotExist:
+            error = "User Does Not Exist"
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+        else:
+            error = "An Error Occured During Login"
+
+    context = {"error": error}
+
+    return render(request, "main/login.html", context)
