3662bebd7084351192b18befed206307131ab540
The Taha Yaseen <thetahayaseen@gmail.com>
2023-11-03 20:22:55 +0500

    Forgot ''

--- AGEPrototype/urls.py
+++ AGEPrototype/urls.py
@@ -17,8 +17,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-import main
-
 urlpatterns = [
-    path("", include(main.urls))
+    path("", include("main.urls"))
 ]
