4aed642dd70bf2b1315ac5ac5cabf12a977c4567
The Taha Yaseen <thetahayaseen@gmail.com>
2023-11-03 20:11:23 +0500

    Introduced Register

---
+++ main/urls.py
@@ -0,0 +1,7 @@
+
+from django.urls import path
+
+
+url_patterns = [
+    path("register", views.register_view, name = "register")
+]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+from django.contrib.auth.forms import UserCreationForm
+
 # Create your views here.
+def register_view(request):
+
+    form = UserCreationForm()
+
+    context = {"form": form}
+
+    return render(request, "main/register.html", context)
