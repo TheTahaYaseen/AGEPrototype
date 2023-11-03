bc8f1b3968e28f34e2771885f810a3af41ba9aa0
The Taha Yaseen <thetahayaseen@gmail.com>
2023-11-04 03:36:26 +0500

    Made New Models So The Site's Specifications Can Be Stored In A Place And Easily Updated

--- main/models.py
+++ main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class SiteSpecificationModel(models.Model):
+    type = models.CharField(max_length=255)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+class SiteSpecificationsDetailModel(models.Model):
+    specification_of = models.ForeignKey(SiteSpecificationModel, on_delete=models.CASCADE)
+    detail_type = models.CharField(max_length=255)
+    detail = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
