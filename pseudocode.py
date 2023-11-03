35470d49321ff4b4d23140f2d606746f19ea4c3e
The Taha Yaseen <thetahayaseen@gmail.com>
2023-11-03 19:47:41 +0500

    The Pseudo-Code

---
+++ pseudocode.py
@@ -0,0 +1,19 @@
+# Pseudo Code
+
+"""
+
+Just Going Through The Wide Scope Tasks And When I Reach One, I Break That Task More And More Until I Reach The First Principles
+
+Get Most Suitable Template
+
+# Tasks
+
+Registration (For Later, When It Will Be Possible To Book Orders From The Site)
+Login (For Later, When It Will Be Possible To Book Orders From The Site)
+About Us (General Information About Company, With Edit Functionality For The Admin)
+Contact Us (Form For Queries, Either It Can Directly Send Text Or Email Message Or Query Can Be Recorded In A Database And Then, Displayed To Admin To Be Solved (2nd Option Best For Now, Additional Response Page May Be Needed; Responses Can Also Be Shown In The Contact Page), With Respond To Query Option For Admins)
+All Products Page (If Has, Then Will Have Singular Product Page | Displaying, All Products With Product Info In Blocks And Ability To Send To Individual Pages, With Edit Or Delete Functionality For Admin)
+Blog (For Company News, First One Will Be AGE Is Launching A Site; Displaying All News With Link To View Full Article, With Edit, Delete Functionality For Admin)
+
+
+"""
