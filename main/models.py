de34af9bc9552764fed66ef3f5c496c60aeb4ef2
The Taha Yaseen <thetahayaseen@gmail.com>
2023-11-04 22:37:04 +0500

    Added Models For Products + Added 2 Fields In Queries

--- main/models.py
+++ main/models.py
@@ -19,3 +19,24 @@ class UserQuery(models.Model):
     subject = models.CharField(max_length=255)
     message = models.TextField()
     admin_response = models.TextField(null=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+class ProductCategory(models.Model):
+    category = models.CharField(max_length=255)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+class Product:
+    product_name = models.CharField(max_length=255)
+    product_description = models.TextField()
+    product_category = models.ForeignKey(ProductCategory, on_delete=models.SET_NULL, null=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+class ProductMedia:
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    media_file = models.FileField(upload_to="product_media/")
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
