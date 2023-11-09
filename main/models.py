108d19333340f028d0e6a311136f7658a2199b07
The Taha Yaseen <thetahayaseen@gmail.com>
2023-11-09 22:41:21 +0500

    Got Rid Of Site Specifcation Thingy! No Longer Needed!

--- main/models.py
+++ main/models.py
@@ -2,18 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class SiteSpecificationModel(models.Model):
-    type = models.CharField(max_length=255)
-    updated = models.DateTimeField(auto_now=True)
-    created = models.DateTimeField(auto_now_add=True)
-
-class SiteSpecificationsDetailModel(models.Model):
-    specification_of = models.ForeignKey(SiteSpecificationModel, on_delete=models.CASCADE)
-    detail_type = models.CharField(max_length=255)
-    detail = models.TextField()
-    updated = models.DateTimeField(auto_now=True)
-    created = models.DateTimeField(auto_now_add=True)
-
 class UserQuery(models.Model):
     query_user = models.ForeignKey(User, on_delete=models.CASCADE)
     subject = models.CharField(max_length=255)
--- main/views.py
+++ main/views.py
@@ -107,19 +107,6 @@ def about_view(request):
 
     return render(request, "main/user_interface/about.html", context)
 
-
-def specifications_admin_view(request):
-    # Create Form 
-    # Fetches All Specifications
-    # Option To Update
-    # Option To Delete
-
-    if not request.user.is_superuser:
-        return redirect("home")
-
-    context = {}
-    return render(request, "main/admin_interface/specifications.html", context)
-
 def contact_view(request):
     
     error = ""
